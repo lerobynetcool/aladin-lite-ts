fe122416a9b46813a7c71c5f1be9b6c2cefcb316
laurent roro <le.robynet.cool@gmail.com>
2021-11-18 11:24:33 +0100

    rewrite TileBuffer as class

--- src/js/TileBuffer.js
+++ src/js/TileBuffer.js
@@ -26,47 +26,33 @@
  * 
  *****************************************************************************/
 
-TileBuffer = (function() {
-	var NB_MAX_TILES = 800; // buffer size
-	
-	// constructor
-	function TileBuffer() {
-		this.pointer = 0;
-		this.tilesMap = {};
-		this.tilesArray = new Array(NB_MAX_TILES);
+let NB_MAX_TILES = 800 // buffer size
 
-		for (var i=0; i<NB_MAX_TILES; i++) {
-			this.tilesArray[i] = new Tile(new Image(), null);
-		}
-	};
+export class TileBuffer {
+
+	pointer = 0
+	tilesMap = {}
+	tilesArray = Array.from({length: NB_MAX_TILES}, () => new Tile(new Image(), null))
 	
-	TileBuffer.prototype.addTile = function(url) {
+	addTile(url) {
 		// return null if already in buffer
-		if (this.getTile(url)) {
-			return null;
-		}
+		if (this.getTile(url)) return null
 
 		// delete existing tile
-		var curTile = this.tilesArray[this.pointer];
+		let curTile = this.tilesArray[this.pointer]
 		if (curTile.url != null) {
-			curTile.img.src = null;
-			delete this.tilesMap[curTile.url];
+			curTile.img.src = ""
+			delete this.tilesMap[curTile.url]
 		}
 
-		this.tilesArray[this.pointer].url = url;
-		this.tilesMap[url] = this.tilesArray[this.pointer];
+		this.tilesArray[this.pointer].url = url
+		this.tilesMap[url] = this.tilesArray[this.pointer]
 
-		this.pointer++;
-		if (this.pointer>=NB_MAX_TILES) {
-			this.pointer = 0;
-		}
+		this.pointer++
+		if (this.pointer>=NB_MAX_TILES) this.pointer = 0
 
-		return this.tilesMap[url];
-	};
-	
-	TileBuffer.prototype.getTile = function(url) {
-		return this.tilesMap[url];
-	};
-	
-	return TileBuffer;
-})();
+		return this.tilesMap[url]
+	}
+
+	getTile(url) { return this.tilesMap[url] }
+}
