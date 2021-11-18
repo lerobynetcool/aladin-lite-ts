b902047c9d21cf51ad1463e9bd299bdfd7663e2d
laurent roro <le.robynet.cool@gmail.com>
2021-11-18 13:47:43 +0100

    some more Basic + Object.prototype.map defined

--- src/Basic.ts
+++ src/Basic.ts
@@ -7,3 +7,28 @@ export type Txy = {
 	x: number
 	y: number
 }
+
+export type mat3 = number[][] // TODO can ts fix size ?
+export type vec3 = number[]   // TODO can ts fix size ?
+export type vec2 = number[]   // TODO can ts fix size ?
+
+export type TXY = {
+	X: number
+	Y: number
+}
+
+export type RaDec = {
+	ra: number
+	dec: number
+}
+
+// useful extension of default object
+// TODO : this is not necessarily best practise, but it really makes sens
+declare global {
+	interface Object {
+		map: <T>(fun: (val: any, key: string) => T ) => T[]
+	}
+}
+Object.prototype.map = function<T>(fun: (val: any, key: string) => T ): T[] {
+	return Object.keys(this).map( key => fun((this as any)[key],key) )
+}
