9201413b0c91160f6aaabaa5d0ef1c5662895383
laurent roro <le.robynet.cool@gmail.com>
2021-11-29 10:11:29 +0100

    Location.js to ts

--- src/Location.ts
+++ src/Location.ts
@@ -26,16 +26,18 @@
  *
  *****************************************************************************/
 
-class Location {
-	constructor(locationDiv) {
-		this.$div = $(locationDiv)
-	}
+import { Coo } from './libs/astro/coo'
+import { CooFrameEnum } from './CooFrameEnum'
 
-	update(lon, lat, cooFrame, isViewCenterPosition = false) {
-		var coo = new Coo(lon, lat, 7)
-		if (cooFrame==CooFrameEnum.J2000)       this.$div.html(coo.format('s/'))
-		else if (cooFrame==CooFrameEnum.J2000d) this.$div.html(coo.format('d/'))
-		else                                    this.$div.html(coo.format('d/'))
+export class Location {
+	$div
+	constructor(locationDiv: any) { this.$div = $(locationDiv) }
+
+	update(lon: number, lat: number, cooFrame: CooFrameEnum, isViewCenterPosition = false) {
+		let coo = new Coo(lon, lat, 7)
+		if (cooFrame==CooFrameEnum.J2000)       this.$div.html(coo.format('s/') as string)
+		else if (cooFrame==CooFrameEnum.J2000d) this.$div.html(coo.format('d/') as string)
+		else                                    this.$div.html(coo.format('d/') as string)
 		this.$div.toggleClass('aladin-reticleColor', isViewCenterPosition)
 	}
 }
