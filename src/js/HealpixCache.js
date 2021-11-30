d67695a65fe256e03b3bd7c72527d3f21fc4fa6e
laurent roro <le.robynet.cool@gmail.com>
2021-11-30 05:18:30 +0100

    cosmetic

--- src/js/HealpixCache.js
+++ src/js/HealpixCache.js
@@ -46,30 +46,30 @@ class HealpixCache {
 
 	static init() {
 		// pre-compute corners position for nside=8
-		var hpxIdx = new HealpixIndex(8);
-		hpxIdx.init();
-		var npix = HealpixIndex.nside2Npix(8);
-		var corners;
+		var hpxIdx = new HealpixIndex(8)
+		hpxIdx.init()
+		var npix = HealpixIndex.nside2Npix(8)
+		var corners
 		for (var ipix=0; ipix<npix; ipix++) {
-			corners =  hpxIdx.corners_nest(ipix, 1);
-			HealpixCache.staticCache.corners.nside8.push(corners);
+			corners =  hpxIdx.corners_nest(ipix, 1)
+			HealpixCache.staticCache.corners.nside8.push(corners)
 		}
 
-		HealpixCache.hpxIdxCache = hpxIdx;
+		HealpixCache.hpxIdxCache = hpxIdx
 	}
 
 	static corners_nest(ipix, nside) {
 		if (nside==8) {
-			return HealpixCache.staticCache.corners.nside8[ipix];
+			return HealpixCache.staticCache.corners.nside8[ipix]
 		}
 
 		if (nside != HealpixCache.lastNside) {
-			HealpixCache.hpxIdxCache = new HealpixIndex(nside);
-			HealpixCache.hpxIdxCache.init();
-			HealpixCache.lastNside = nside;
+			HealpixCache.hpxIdxCache = new HealpixIndex(nside)
+			HealpixCache.hpxIdxCache.init()
+			HealpixCache.lastNside = nside
 		}
 
-		return HealpixCache.hpxIdxCache.corners_nest(ipix, 1);
+		return HealpixCache.hpxIdxCache.corners_nest(ipix, 1)
 	}
 
 }
