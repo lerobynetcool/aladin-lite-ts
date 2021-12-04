eead907483363d776a5fc4b0cda0060c52cde712
laurent roro <le.robynet.cool@gmail.com>
2021-12-04 09:32:36 +0100

    MOC.js to ts

--- src/MOC.ts
+++ src/MOC.ts
@@ -9,9 +9,23 @@
  *
  *****************************************************************************/
 
-function log2(val) { return Math.log(val) / Math.LN2 }
+import { Utils, uniq } from './Utils'
+import { Aladin } from './Aladin'
+import { AladinUtils } from './AladinUtils'
+import { CooFrameEnum } from './CooFrameEnum'
+import { CooConversion } from './CooConversion'
+import { Color } from './Color'
+import { View } from './View'
+import { Projection } from './libs/astro/projection'
+import { TXY, Vxy, map } from './Basic'
+import { HealpixCache } from './HealpixCache'
+import { HealpixIndex, SpatialVector } from './Healpix'
 
-function drawCorners(ctx, xyCorners) {
+import { FITS } from './libs/astro/fits'
+
+function log2(x: number) { return Math.log(x)/Math.LN2 }
+
+function drawCorners(ctx: CanvasRenderingContext2D, xyCorners: Vxy[]) {
 	ctx.moveTo(xyCorners[0].vx, xyCorners[0].vy)
 	ctx.lineTo(xyCorners[1].vx, xyCorners[1].vy)
 	ctx.lineTo(xyCorners[2].vx, xyCorners[2].vy)
@@ -19,19 +33,16 @@ function drawCorners(ctx, xyCorners) {
 	ctx.lineTo(xyCorners[0].vx, xyCorners[0].vy)
 }
 
-// remove duplicate items from array a
-function uniq(a) { return [...new Set(self)]}
-
 // TODO: merge with what is done in View.getVisibleCells
 let spVec = new SpatialVector()
-function getXYCorners(nside, ipix, viewFrame, surveyFrame, width, height, largestDim, zoomFactor, projection) {
+function getXYCorners(nside: number, ipix: number, viewFrame: any, surveyFrame: any, width: number, height: number, largestDim: number, zoomFactor: number, projection: Projection) {
 	let cornersXYView = []
-	let cornersXY = []
+	let cornersXY: TXY[] = []
 
 	let corners = HealpixCache.corners_nest(ipix, nside)
 	for (let k=0; k<4; k++) {
 		spVec.setXYZ(corners[k].x, corners[k].y, corners[k].z)
-		let radec = [spVec.ra(), spVec.dec()]
+		let radec: number[] = [spVec.ra(),spVec.dec()]
 		// need for frame transformation ?
 		if (surveyFrame && surveyFrame.system != viewFrame.system) {
 			if (surveyFrame.system == CooFrameEnum.SYSTEMS.J2000)    radec = CooConversion.J2000ToGalactic(radec)
@@ -42,7 +53,7 @@ function getXYCorners(nside, ipix, viewFrame, surveyFrame, width, height, larges
 		else return null
 	}
 
-	for (var k=0; k<4; k++) {
+	for (let k=0; k<4; k++) {
 		cornersXYView[k] = AladinUtils.xyToView(cornersXY[k].X, cornersXY[k].Y, width, height, largestDim, zoomFactor)
 	}
 
@@ -57,21 +68,27 @@ function getXYCorners(nside, ipix, viewFrame, surveyFrame, width, height, larges
 	return cornersXYView
 }
 
-class MOC {
-	type = 'moc'
-
+export class MOC {
+	color: string
+	name: string
+	opacity: number
+	lineWidth: number // TODO or string ?
+	adaptativeDisplay: boolean
 	proxyCalled = false // this is a flag to check whether we already tried to load the MOC through the proxy
 	nbCellsDeepestLevel = 0 // needed to compute the sky fraction of the MOC
 
 	// index of MOC cells at high and low resolution
-	_highResIndexOrder3 = Array.from({length: 768}, () => {return{}}) // an array filled with 768 empty element {}
-	_lowResIndexOrder3  = Array.from({length: 768}, () => {return{}}) // an array filled with 768 empty element {}
+	_highResIndexOrder3: {[key: string]: any[]}[] = Array.from({length: 768}, () => {return{}}) // an array filled with 768 empty element {}
+	_lowResIndexOrder3 : {[key: string]: any[]}[] = Array.from({length: 768}, () => {return{}}) // an array filled with 768 empty element {}
 
 	isShowing = true
 	ready = false
 
-	constructor(options = {}) {
-		this.order = undefined
+	order: number
+	type = 'moc'
+
+	constructor(options: any = {}) {
+		this.order = undefined as any // TODO : seems like a bad idea
 		// TODO homogenize options parsing for all kind of overlay (footprints, catalog, MOC)
 		this.name = options.name || "MOC"
 		this.color = options.color || Color.getNextColor()
@@ -91,13 +108,13 @@ class MOC {
 	static PIVOT_FOV = 30 // when do we switch from low res cells to high res cells (fov in degrees)
 
 	// at end of parsing, we need to remove duplicates from the 2 indexes
-	removeDuplicatesFromIndexes() {
-		this._highResIndexOrder3 = this._highResIndexOrder3.map( o => { for(let k in o) o[k] = uniq(o[k]) } )
-		this._lowResIndexOrder3  = this._lowResIndexOrder3 .map( o => { for(let k in o) o[k] = uniq(o[k]) } )
+	_removeDuplicatesFromIndexes() {
+		this._highResIndexOrder3 = this._highResIndexOrder3.map( o => map(o, (v: any[]) => uniq(v) ) )
+		this._lowResIndexOrder3  = this._lowResIndexOrder3 .map( o => map(o, (v: any[]) => uniq(v) ) )
 	}
 
 	// add pixel (order, ipix)
-	_addPix(order, ipix) {
+	_addPix(order: number, ipix: number) {
 		let ipixOrder3 = Math.floor( ipix * Math.pow(4, (3 - order)) )
 		// fill low and high level cells
 		// 1. if order <= LOWRES_MAXORDER, just store value in low and high res cells
@@ -149,22 +166,23 @@ class MOC {
 	 * set MOC data by parsing a MOC serialized in JSON
 	 * (as defined in IVOA MOC document, section 3.1.1)
 	 */
-	dataFromJSON(jsonMOC) {
+	dataFromJSON(jsonMOC: any) {
 		for (let orderStr in jsonMOC) {
 			if (jsonMOC.hasOwnProperty(orderStr)) {
-				let order = parseInt(orderStr);
+				let order = parseInt(orderStr)
 				if (this.order===undefined || order > this.order) this.order = order
-				jsonMOC[orderStr].forEach( ipix => this._addPix(order, ipix) )
+				jsonMOC[orderStr]?.forEach( (ipix: number) => this._addPix(order, ipix) )
 			}
 		}
 		this.reportChange()
 		this.ready = true
 	}
 
+	dataURL: string|undefined
 	/**
 	 * set MOC data by parsing a URL pointing to a FITS MOC file
 	 */
-	dataFromFITSURL(mocURL, successCallback = ()=>{}) {
+	dataFromFITSURL(mocURL: string, successCallback = ()=>{}) {
 		let self = this
 		let callback = function() {
 			// note: in the callback, 'this' refers to the FITS instance
@@ -177,8 +195,8 @@ class MOC {
 				if (this.hdus.length == 0) {
 					if (self.proxyCalled !== true) {
 						self.proxyCalled = true
-						let proxiedURL = Aladin.JSONP_PROXY + '?url=' + encodeURIComponent(self.dataURL)
-						new astro.FITS(proxiedURL, callback)
+						let proxiedURL = `${Aladin.JSONP_PROXY}?url=${encodeURIComponent(self.dataURL as string)}`
+						new FITS(proxiedURL, callback)
 					}
 					return
 				}
@@ -190,7 +208,7 @@ class MOC {
 			}
 			let hdr1 = this.getHeader(1)
 
-			if (hdr0.contains('HPXMOC'))        self.order = hdr0.get('HPXMOC')
+			     if (hdr0.contains('HPXMOC'))   self.order = hdr0.get('HPXMOC')
 			else if (hdr0.contains('MOCORDER')) self.order = hdr0.get('MOCORDER')
 			else if (hdr1.contains('HPXMOC'))   self.order = hdr1.get('HPXMOC')
 			else if (hdr1.contains('MOCORDER')) self.order = hdr1.get('MOCORDER')
@@ -201,13 +219,13 @@ class MOC {
 
 			let data = this.getDataUnit(1)
 			let colName = data.columns[0]
-			data.getRows(0, data.rows, function(rows) {
-				for (let k=0; k<rows.length; k++) {
-					let uniq = rows[k][colName]
+			data.getRows(0, data.rows, function(rows: any[][]) {
+				rows.forEach( row => {
+					let uniq = row[colName]
 					let order = Math.floor(Math.floor(log2(Math.floor(uniq/4))) / 2)
 					let ipix = uniq - 4 *(Math.pow(4, order))
 					self._addPix(order, ipix)
-				}
+				})
 			})
 			data = null // this helps releasing memory
 			self._removeDuplicatesFromIndexes()
@@ -219,21 +237,22 @@ class MOC {
 		this.dataURL = mocURL
 
 		// instantiate the FITS object which will fetch the URL passed as parameter
-		new astro.FITS(this.dataURL, callback)
+		new FITS(this.dataURL, callback)
 	}
 
-	setView(view) {
+	view?: View
+	setView(view: View) {
 		this.view = view
 		this.reportChange()
 	}
 
-	draw(ctx, projection, viewFrame, width, height, largestDim, zoomFactor, fov) {
+	draw(ctx: CanvasRenderingContext2D, projection: Projection, viewFrame: any, width: number, height: number, largestDim: number, zoomFactor: number, fov: number) {
 		if (!this.isShowing || !this.ready) return
 		let mocCells = fov > MOC.PIVOT_FOV && this.adaptativeDisplay ? this._lowResIndexOrder3 : this._highResIndexOrder3
 		this._drawCells(ctx, mocCells, fov, projection, viewFrame, CooFrameEnum.J2000, width, height, largestDim, zoomFactor)
 	}
 
-	_drawCells(ctx, mocCellsIdxOrder3, fov, projection, viewFrame, surveyFrame, width, height, largestDim, zoomFactor) {
+	_drawCells(ctx: CanvasRenderingContext2D, mocCellsIdxOrder3: any[], fov: any, projection: Projection, viewFrame: any, surveyFrame: any, width: number, height: number, largestDim: number, zoomFactor: number) {
 		ctx.lineWidth = this.lineWidth
 		// if opacity==1, we draw solid lines, else we fill each HEALPix cell
 		if (this.opacity==1) ctx.strokeStyle = this.color
@@ -244,7 +263,7 @@ class MOC {
 
 		ctx.beginPath()
 
-		let orderedKeys = []
+		let orderedKeys: number[] = []
 		for (let k=0; k<768; k++) {
 			let mocCells = mocCellsIdxOrder3[k]
 			for (let key in mocCells) orderedKeys.push(parseInt(key))
@@ -252,7 +271,7 @@ class MOC {
 		orderedKeys.sort(function(a, b) {return a - b;})
 		let norderMax = orderedKeys[orderedKeys.length-1]
 
-		let potentialVisibleHpxCellsOrder3 = this.view.getVisiblePixList(3, CooFrameEnum.J2000)
+		let potentialVisibleHpxCellsOrder3 = this.view?.getVisiblePixList(3, CooFrameEnum.J2000) || []
 
 		// let's test first all potential visible cells and keep only the one with a projection inside the view
 		let visibleHpxCellsOrder3 = potentialVisibleHpxCellsOrder3
@@ -295,7 +314,7 @@ class MOC {
 		}
 	}
 
-	reportChange() { this.view && this.view.requestRedraw() }
+	reportChange() { this.view?.requestRedraw() }
 
 	show() {
 		if (this.isShowing) return
@@ -312,12 +331,11 @@ class MOC {
 	// Tests whether a given (ra, dec) point on the sky is within the current MOC object
 	//
 	// returns true if point is contained, false otherwise
-	contains(ra, dec) {
+	contains(ra: number, dec: number) {
 		let hpxIdx = new HealpixIndex(Math.pow(2, this.order))
-		hpxIdx.init()
 		let polar = Utils.radecToPolar(ra, dec)
 		let ipix = hpxIdx.ang2pix_nest(polar.theta, polar.phi)
-		let ipixMapByOrder = {}
+		let ipixMapByOrder: any = {}
 		for (let curOrder=0; curOrder<=this.order; curOrder++) {
 			ipixMapByOrder[curOrder] = Math.floor(ipix / Math.pow(4, this.order - curOrder))
 		}
@@ -325,7 +343,7 @@ class MOC {
 		for (let ipixOrder3=0; ipixOrder3<768; ipixOrder3++) {
 			let mocCells = this._highResIndexOrder3[ipixOrder3]
 			for (let order in mocCells) {
-				if (order<3) {
+				if (Number(order)<3) {
 					for (let k=mocCells[order].length; k>=0; k--) {
 						if (ipixMapByOrder[order] == mocCells[order][k]) return true
 					}
